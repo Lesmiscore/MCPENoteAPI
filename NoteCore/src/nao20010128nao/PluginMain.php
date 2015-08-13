fa19a125bd22df7a1da5cae39e12a94a425649cb
nao20010128nao <nao20010128@gmail.com>
2015-08-13 09:59:58 +0900

    [コア]完成?

--- NoteCore/src/nao20010128nao/PluginMain.php
+++ NoteCore/src/nao20010128nao/PluginMain.php
@@ -5,13 +5,37 @@ namespace nao20010128nao;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
+use pocketmine\level\sound\LaunchSound;
 
 class PluginMain extends PluginBase implements Listener
 {
+	public const C=100;
+	public const D=200;
+	public const E=300;
+	public const F=400;
+	public const G=500;
+	public const A=600;
+	public const B=700;
 	public function onEnable(){
 		@mkdir($this->getDataFolder());
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	public function onDisable(){
+	}
+	public function playForAll($pitch){
+		foreach($this->getServer()->getOnlinePlayers() as $player){
+			$pos=$player->getPosition();
+			$level=$pos->getLevel();
+			$level->addSound(new LaunchSound($pos,$pitch));
+		}
+	}
+	public function playFor($pitch,$players){
+		if(!is_array($players))
+			$players=array($players);
+		foreach($players as $player){
+			$pos=$player->getPosition();
+			$level=$pos->getLevel();
+			$level->addSound(new LaunchSound($pos,$pitch));
+		}
 	}
 }
